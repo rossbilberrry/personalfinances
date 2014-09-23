44e97e7113d32fc6d7fbe6eb0fd02f4fa7d4a25f
Ross Dzikovskyy <ross.bilberrry@gmail.com>
2014-09-23 18:02:08 +0300

    fixed migrations

--- db/migrate/20140923144459_add_name_to_transaction.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToTransaction < ActiveRecord::Migration
-  def change
-    add_column :transactions, :name, :string
-  end
-end
--- db/migrate/20140923144735_add_amount_to_transactions.rb
+++
@@ -1,5 +0,0 @@
-class AddAmountToTransactions < ActiveRecord::Migration
-  def change
-    add_column :transactions, :amount, :integer
-  end
-end
